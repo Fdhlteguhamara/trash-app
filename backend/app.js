af8588d1bb2389449dfc9688f9c03747cb4a9239
Fdhlteguhamara <fadhil.teguh@mhs.itenas.ac.id>
2026-04-17 01:41:45 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,23 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// 🔥 serve frontend
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// 🔥 API
 const reportRoutes = require('./routes/reportRoutes');
 app.use('/api', reportRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Trash App API Running');
+// 🔥 fallback ke index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(3000, () => {
